87d3d88389cb87ead53058d77623582193a7eabf
Higo Thaian <higo.thaian@gmail.com>
2025-09-23 21:09:43 -0300

    Radalt antenna implementation

---
+++ sharc/antenna/antenna_radalt_m2319.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 14 14:13:58 2017
+
+@author: Calil
+"""
+
+import numpy as np
+from sharc.antenna.antenna import Antenna
+from sharc.support.named_tuples import AntennaPar
+
+
+class AntennaRadaltM2319(Antenna):
+    """
+    Implement the Radalt antenna pattern.
+
+    Attributes
+    ----------
+        g_max (float): maximum antenna gain
+        phi_3db (float): 3dB beamwidth of single element [degrees]
+    """
+    
+    def __init__(self, par: AntennaPar):
+        """
+        Constructs an AntennaRadalt object.
+
+        Parameters
+        ---------
+            par (AntennaPar): antenna parameters
+        """
+
+        self.param = par
+
+        self.g_max = par.element_max_g
+        self.phi_3dB = par.element_phi_3db
+
+    def calculate_gain(self, phi: np.array, theta: np.array) -> np.array:
+        """
+        Calculates the Radalt radiation pattern.
+
+        Parameters
+        ----------
+            phi (np.array): elevation angle [degrees]
+            theta (np.array): azimuth angle [degrees]
+
+        Returns
+        -------
+            gain (np.array): Radalt radiation pattern gain value
+        """
+
+        gain = -( 12.0 / self.phi_3dB**2 ) * phi**2 + self.g_max
+        
+        return gain
